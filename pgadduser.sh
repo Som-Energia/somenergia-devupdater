f272fff1a031fbaf4d7cd385cdc8adb648702eba
David García Garzón <voki@canvoki.net>
2019-11-07 21:06:40 +0100

    pgadduser: obtain hba file from psql

--- pgadduser.sh
+++ pgadduser.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 user=$1
+hbafile=$(psql -At -c 'SHOW hba_file')
 
 if [ "$USER" != "postgres" ]; then
 	echo should be run as postgres user
 	exit -1
 fi
 
-echo "local all '${user}' peer" >> /etc/postgresql/*/main/pg_hba.conf
+echo "local all '${user}' peer" >> $hbafile
 createuser -w -s "${user}"
 
 
